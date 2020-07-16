84c64e405cf85c647e764c6006cc62c2a6efd343
Parshuram Kumar Gupta <54715150+parshuram14268@users.noreply.github.com>
2020-07-16 13:44:30 +0530

    Update Binary Queries.py

--- Binary Queries.py
+++ Binary Queries.py
@@ -3,10 +3,7 @@ s=list(map(int,input().split()))
 for i in range(q):
     arr=list(map(int,input().split()))
     if arr[0] is 1:
-        if s[arr[1]-1]==0:
-            s[arr[1]-1]=1
-        else:
-            s[arr[1]-1]=0
+        s[arr[1]-1]=(s[arr[1]-1])^1
     else:
         if s[arr[2]-1] == 0:
             print("EVEN")
